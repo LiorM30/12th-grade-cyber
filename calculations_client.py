ad0106157a0336cb52949906a428a36e6145b97a
LiorM30 <87566719+LiorM30@users.noreply.github.com>
2022-10-27 21:31:20 +0300

    Add files via upload

---
+++ calculations_client.py
@@ -0,0 +1,11 @@
+import socket
+import struct
+import logging
+
+
+class Calculations_Client:
+    def __init__(self) -> None:
+        self.logger = logging.getLogger('root')
+
+    def run():
+        raise NotImplementedError
